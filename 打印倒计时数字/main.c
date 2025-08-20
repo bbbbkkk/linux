697db43948aa605ef1092f5e6958eaed48373f42
bbbkkk <sbkhao666@gmail.com>
2025-08-20 13:24:25 -0700

    Update main.c

--- 打印倒计时数字/main.c
+++ 打印倒计时数字/main.c
@@ -1,1 +1,16 @@
+#include "processbar.h"
+#include <unistd.h>
+int main()
+{
+    int cnt= 10;
+    while(cnt>=0)
+    {
+        printf("%-2d\r",cnt);
+        fflush(stdout);//立即刷新到屏幕上
+        cnt--;
+        sleep(1);//睡眠一秒
+    }
+    printf("\n");
+    return 0;
 
+}
